eef60d75855a4aecf056a334f59c09b4a432eab6
Jorge Martins <jorge.martins@shattereddev.com>
2023-05-01 00:22:01 +0100

    finish up idea

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}, s
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (tx, rx) = broadcast::channel::<String>(10);
+    let (tx, rx) = broadcast::channel(10);
 
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
@@ -21,12 +21,15 @@ async fn main() {
                     bytes_read = reader.read_line(&mut line) => {
                         if bytes_read.unwrap() == 0 { break; }
     
-                        tx.send(line.clone()).unwrap();
+                        tx.send((line.clone(), addr)).unwrap();
                         line.clear();
                     }
                     rcv_data = rx.recv() => {
-                        let msg = rcv_data.unwrap();
-                        write.write_all(msg.as_bytes()).await.unwrap();
+                        let (msg, other_addr) = rcv_data.unwrap();
+                        
+                        if other_addr != addr {
+                            write.write_all(msg.as_bytes()).await.unwrap();
+                        }
                     }
                 }
             }
