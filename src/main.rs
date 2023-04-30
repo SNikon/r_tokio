afc91307a9d54f8e45751e8208ff420c35e3a889
Jorge Martins <jorge.martins@shattereddev.com>
2023-05-01 00:03:46 +0100

    introducing tokio::select

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}};
+use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}, sync::broadcast};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, addr) = listener.accept().await.unwrap();
 
-    let (read, mut write) = socket.split();
-    let mut reader = BufReader::new(read);
-    let mut line = String::new();
+    let (tx, rx) = broadcast::channel::<String>(10);
 
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 { break; }
-        write.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (read, mut write) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+    
+            loop {
+                tokio::select! {
+                    bytes_read = reader.read_line(&mut line) => {
+                        if bytes_read.unwrap() == 0 { break; }
+    
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
+                    rcv_data = rx.recv() => {
+                        let msg = rcv_data.unwrap();
+                        write.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                }
+            }
+        });
     }
 }
